cdbc0d86158c3d2aaef06f790b2d54f58419a209
ArtCosGRP <artcosgroup@gmail.com>
2024-08-26 13:58:45 +0500

    wieu

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '@/pages/Home/Home.vue';
 import Menu from '@/pages/Menu/Menu.vue';
 import Breakfast from '@/pages/Breakfast/Breakfast.vue';
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     { path: '/', name: 'homepage', component: Home },
     { path: '/menu', name: 'menu', component: Menu },

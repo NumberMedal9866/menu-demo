a566fa521b34125f8074c0f125659b2d3c7cdfe2
ArtCosGRP <artcosgroup@gmail.com>
2024-08-26 13:45:56 +0500

    wewrewrewerwer

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import {URL, fileURLToPath} from "node:url"
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { URL, fileURLToPath } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,10 +15,8 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['pinia']
-    },
-    rollupOptions: {
-      external: ['vue-router']
+      // Add all external dependencies here
+      external: ['pinia', 'vue-router'],
     }
   }
-})
+});
